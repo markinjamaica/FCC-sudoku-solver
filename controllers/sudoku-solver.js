253d7ef4f3df5158778ff64245ee36605534fa8a
Mark Gauffin <markgauffin@gmail.com>
2022-02-05 06:36:08 -0500

    add validate method

--- controllers/sudoku-solver.js
+++ controllers/sudoku-solver.js
@@ -1,24 +1,26 @@
 class SudokuSolver {
+    validate(puzzleString) {
+        const validRegex = /^([1-9\.]{81})$/;
+        console.log(validRegex.test(puzzleString));
+        if (!validRegex.test(puzzleString)) {
+            return false;
+        }
+        return true;
+    }
 
-  validate(puzzleString) {
-  }
+    checkRowPlacement(puzzleString, row, column, value) {}
 
-  checkRowPlacement(puzzleString, row, column, value) {
+    checkColPlacement(puzzleString, row, column, value) {}
 
-  }
+    checkRegionPlacement(puzzleString, row, column, value) {}
 
-  checkColPlacement(puzzleString, row, column, value) {
-
-  }
-
-  checkRegionPlacement(puzzleString, row, column, value) {
-
-  }
-
-  solve(puzzleString) {
-    
-  }
+    solve(puzzleString) {}
 }
+// const puzzle = new SudokuSolver();
+// console.log(
+//     puzzle.validate(
+//         '1.5..2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37.'
+//     )
+// );
 
 module.exports = SudokuSolver;
-
