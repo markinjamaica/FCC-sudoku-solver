29b8bc7720df7ea0471d43e2cdbbe299ef006ff4
Mark Gauffin <markgauffin@gmail.com>
2022-02-08 05:55:27 -0500

    feat: add checkColPlacement function

--- controllers/sudoku-solver.js
+++ controllers/sudoku-solver.js
@@ -24,17 +24,34 @@ class SudokuSolver {
             }
             rows[letter].push(puzzleString[i]);
         }
+
+        // Check to see if value already exists in row, or spot already taken
         if (rows[row].includes(value) || rows[row][column - 1] !== '.') {
             return false;
         }
         return true;
-        // if value is equal existing value in row, invalid
-        // example: row = A, column = 1, value = 6
-        // push puzzlestring values into selected row array
     }
 
     checkColPlacement(puzzleString, row, column, value) {
-        // if value is equal to existing value in column, invalid
+        const columns = {};
+
+        // Populate columns object with property numbers 1-9
+        for (let i = 0; i < 81; i++) {
+            let number = (i % 9) + 1;
+            if (!columns[number]) {
+                columns[number] = [];
+            }
+            columns[number].push(puzzleString[i]);
+        }
+
+        // Check to see if value already exists in column, or spot already taken
+        if (
+            columns[column].includes(value) ||
+            columns[column][row.charCodeAt(0) - 'A'.charCodeAt(0)] !== '.'
+        ) {
+            return false;
+        }
+        return true;
     }
 
     checkRegionPlacement(puzzleString, row, column, value) {
@@ -44,12 +61,12 @@ class SudokuSolver {
     solve(puzzleString) {}
 }
 const puzzle = new SudokuSolver();
-const row = 'A';
-const column = 3;
-const value = 6;
+const row = 'B';
+const column = 2;
+const value = '1';
 const puzzleString =
     '1.5..2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37.';
-const result = puzzle.checkRowPlacement(puzzleString, row, column, value);
+const result = puzzle.checkColPlacement(puzzleString, row, column, value);
 console.log(result);
 
 module.exports = SudokuSolver;
