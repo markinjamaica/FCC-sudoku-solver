2d1fa972dd73f3685937be92d90b82ed05bf984e
Mark Gauffin <markgauffin@gmail.com>
2022-02-08 05:18:15 -0500

    feat: add checkRowPlacement function

--- controllers/sudoku-solver.js
+++ controllers/sudoku-solver.js
@@ -12,7 +12,25 @@ class SudokuSolver {
     }
 
     checkRowPlacement(puzzleString, row, column, value) {
+        const rows = {};
+
+        // Populate rows object with property names A - I
+        for (let i = 0; i < 81; i++) {
+            let letter = String.fromCharCode(
+                'A'.charCodeAt(0) + Math.floor(i / 9)
+            );
+            if (!rows[letter]) {
+                rows[letter] = [];
+            }
+            rows[letter].push(puzzleString[i]);
+        }
+        if (rows[row].includes(value) || rows[row][column - 1] !== '.') {
+            return false;
+        }
+        return true;
         // if value is equal existing value in row, invalid
+        // example: row = A, column = 1, value = 6
+        // push puzzlestring values into selected row array
     }
 
     checkColPlacement(puzzleString, row, column, value) {
@@ -26,9 +44,12 @@ class SudokuSolver {
     solve(puzzleString) {}
 }
 const puzzle = new SudokuSolver();
-const result = puzzle.validate(
-    '1.5..2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37.'
-);
+const row = 'A';
+const column = 3;
+const value = 6;
+const puzzleString =
+    '1.5..2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37.';
+const result = puzzle.checkRowPlacement(puzzleString, row, column, value);
 console.log(result);
 
 module.exports = SudokuSolver;
