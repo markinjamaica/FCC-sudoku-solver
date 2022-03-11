b6f1baf647f09e17422cf947ab4a2b78800a02cb
Mark Gauffin <markgauffin@gmail.com>
2022-03-11 05:42:33 -0500

    test: solve a puzzle with valid puzzle string

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -1,4 +1,4 @@
-const chai = require("chai");
+const chai = require('chai');
 const chaiHttp = require('chai-http');
 const assert = chai.assert;
 const server = require('../server');
@@ -6,6 +6,16 @@ const server = require('../server');
 chai.use(chaiHttp);
 
 suite('Functional Tests', () => {
-
+    test('Solve a puzzle with valid puzzle string', function (done) {
+        chai.request(server)
+            .post('/api/solve')
+            .send(
+                '..839.7.575.....964..1.......16.29846.9.312.7..754.....62..5.78.8...3.2...492...1'
+            )
+            .end(function (err, res) {
+                assert.equal(res.status, 200);
+                assert.equal(res.text, 'hello Guest');
+                done();
+            });
+    });
 });
-
