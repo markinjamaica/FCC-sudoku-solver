5dc399d95455df0b79da8611dc882fe5427c19d9
Mark Gauffin <markgauffin@gmail.com>
2022-03-14 06:13:12 -0500

    test: fix test solve a puzzle w/ valid puzzle string

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -9,12 +9,15 @@ suite('Functional Tests', () => {
     test('Solve a puzzle with valid puzzle string', function (done) {
         chai.request(server)
             .post('/api/solve')
-            .send(
-                '..839.7.575.....964..1.......16.29846.9.312.7..754.....62..5.78.8...3.2...492...1'
-            )
+            .send({
+                puzzle: '..839.7.575.....964..1.......16.29846.9.312.7..754.....62..5.78.8...3.2...492...1',
+            })
             .end(function (err, res) {
                 assert.equal(res.status, 200);
-                assert.equal(res.text, 'hello Guest');
+                assert.equal(
+                    res.body.solution,
+                    '218396745753284196496157832531672984649831257827549613962415378185763429374928561'
+                );
                 done();
             });
     });
